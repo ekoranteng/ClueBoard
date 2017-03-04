e5f44f10b01d70f1a1e9ad964f90745f5c7f8c1d
Coleman Hoyt <choyt@mines.edu>
2017-03-04 16:12:44 -0700

    BadConfigException has no instances now. Added CR adjacency tests

--- src/clueGame/BadConfigFormatException.java
+++ src/clueGame/BadConfigFormatException.java
@@ -1,13 +1,8 @@
 package clueGame;
 public class BadConfigFormatException extends Exception {
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
-	private String badFile;
 	public BadConfigFormatException(String badFile) {
 		super();
-		this.badFile = badFile;
 		System.out.println("Bad File: " + badFile);
 	}
 }
